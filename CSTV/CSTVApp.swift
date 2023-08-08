1b8debe88cdd9495dd48699277419341ede35489
Mateus Marques <marques.m05@gmail.com>
2023-08-08 16:08:49 -0300

    Initial Commit

---
+++ CSTV/CSTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVApp.swift
+//  CSTV
+//
+//  Created by Mateus Marques on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CSTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
