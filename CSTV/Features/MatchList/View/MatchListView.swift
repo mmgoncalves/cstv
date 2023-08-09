5022ce0bcb55cf3312c110988d55275ffd59fa90
Mateus Marques <marques.m05@gmail.com>
2023-08-09 17:26:46 -0300

    Create LoadingView component

---
+++ CSTV/Features/MatchList/View/MatchListView.swift
@@ -0,0 +1,21 @@
+//
+//  MatchListView.swift
+//  CSTV
+//
+//  Created by Mateus Marques on 09/08/23.
+//
+
+import SwiftUI
+
+struct MatchListView: View {
+    
+    var body: some View {
+        LoadingView()
+    }
+}
+
+struct MatchListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchListView()
+    }
+}
